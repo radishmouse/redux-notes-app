142cbc357a8ed97a94d8e4f54e63bc2a4a8cc89d
Chris Aquino <chris.aquino@gmail.com>
2018-12-13 10:28:41 -0500

    sets up default state, action, and action creator. begins reducer

--- store.js
+++ store.js
@@ -7,15 +7,44 @@ const { createStore } = require('redux');
 // ================================================
 
 // #1 - Describe the state
+const defaultState = {
+    note: {
+        content: ''
+    }
+}
 
 // #2 - List out all the kinds/types of changes you could make to state
+/*
+- UPDATE_CONTENT    
+*/
+const ACTION_UPDATE = {
+    type: 'ACTION_UPDATE',
+    // content: '??????'
+}
 
 // #3 - Write action creator functions
 // These are functions that use/return those descriptions
 // of changes you can make to state.
+const updateContent = (content) => {
+    return {
+        ...ACTION_UPDATE,
+        content
+    }
+}
+
 
 // #4 - Write a reducer function that accepts state and an action
 // and returns the next version of the state
+const note = (state=defaultState, action) => {
+    switch(action.type) {
+        case ACTION_UPDATE.type:
+            
+
+        default:
+            return state;
+    }
+}
+
 
 // #5 - Create a store that uses your reducer
 
