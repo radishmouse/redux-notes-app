0e912ace3417425a38acce9b262e7b4baedeae0b
Chris Aquino <chris.aquino@gmail.com>
2018-12-13 10:19:33 -0500

    describes steps and splits into two files

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('yay redux');
+const store = require('./store');
+
+// ================================================
+// APP
+// ================================================
+
+// #6 - set up subscriptions for notifications of when the state changes
+// and perform changes to the state
---
+++ store.js
@@ -0,0 +1,21 @@
+// console.log('yay redux');
+const { createStore } = require('redux');
+
+
+// ================================================
+// SETUP
+// ================================================
+
+// #1 - Describe the state
+
+// #2 - List out all the kinds/types of changes you could make to state
+
+// #3 - Write action creator functions
+// These are functions that use/return those descriptions
+// of changes you can make to state.
+
+// #4 - Write a reducer function that accepts state and an action
+// and returns the next version of the state
+
+// #5 - Create a store that uses your reducer
+
